0abec0760d726025d245bec2a6d0ed7579762715
Blue Teeth <daominhtri1000@gmail.com>
2023-08-15 10:17:01 +0700

    Update UserService

---
+++ Sample ASP.NET CORE/Sample/Domain/Repositories/Implements/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Data;
+using Domain.Repositories.Interface;
+using Infrastructure.Repositories.Implements;
+using Sample.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories.Implements
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+
+    }
+}
---
+++ Sample ASP.NET CORE/Sample/Domain/Repositories/Interface/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Repositories.Interface;
+using Sample.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories.Interface
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+    }
+}
